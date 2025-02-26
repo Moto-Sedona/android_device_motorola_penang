f6632bc6df3454c3725106d8849434a453230ceb
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-02-26 23:03:06 +0900

    penang: extract-utils: Sync with common

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -2,75 +2,21 @@
 #
 # SPDX-FileCopyrightText: 2016 The CyanogenMod Project
 # SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
-DEVICE=penang
-VENDOR=motorola
+export DEVICE=penang
+export DEVICE_COMMON=sm6375-common
+export VENDOR=motorola
+export VENDOR_COMMON=${VENDOR}
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-function vendor_imports() {
-    cat <<EOF >>"$1"
-        "device/motorola/penang",
-        "vendor/motorola/sm6375-common",
-        "vendor/qcom/opensource/display",
-EOF
-}
-
-function lib_to_package_fixup_vendor_variants() {
-    if [ "$2" != "vendor" ]; then
-        return 1
-    fi
-
-    case "$1" in
-#        com.qualcomm.qti.dpm.api@1.0 | \
-#            libmmosal | \
-#            vendor.qti.diaghal@1.0 | \
-#            vendor.qti.hardware.fm@1.0 | \
-#            vendor.qti.hardware.wifidisplaysession@1.0 | \
-#            vendor.qti.imsrtpservice@3.0 | \
-#            vendor.qti.qspmhal@1.0)
-#            echo "$1_vendor"
-#            ;;
-#        libofflinelog | \
-#            libqsap_sdk | \
-#            libthermalclient | \
-#            libwpa_client) ;;
-        *)
-            return 1
-            ;;
-    esac
-}
-
-function lib_to_package_fixup() {
-    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
-        lib_to_package_fixup_proto_3_9_1 "$1" ||
-        lib_to_package_fixup_vendor_variants "$@"
-}
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Warning headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt"
-
-#append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
-
-# Finish
-write_footers
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
